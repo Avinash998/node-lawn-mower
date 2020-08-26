db52e70a784e6c94d7ea0868ef144eb3f29316ae
Avinash <avinashgolu.chourasia@gmail.com>
2020-08-26 09:56:58 +0530

    express setup

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Lawn Mower");
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/test', (req,res) => {
+    console.log("Test Succfull");
+    res.status(200).send("Test successfull");
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening to port ${PORT}`);
+})
